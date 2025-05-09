bc5eed743ea8e52a3ba087e4cff443b4ecec70a1
Manami Tsukada <manami.tsukada3@gmail.com>
2025-05-09 09:01:36 +0100

    added

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,38 +1,35 @@
 class BookmarksController < ApplicationController
-  before_action :set_bookmark, only: :destroy
-  before_action :set_list, only: %i[new create]
+  before_action :set_list, only: [:new, :create]
 
   def new
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
-    @bookmark = Bookmark.new(bookmark_params)
-    @bookmark.list = @list
+    @list = List.find(params[:list_id])
+    @bookmark = @list.bookmarks.build(bookmark_params)
+  
     if @bookmark.save
-      redirect_to list_path(@list)
+      redirect_to @list, notice: 'Bookmark was successfully created.'
     else
-      @review = Review.new
-      render :new, status: :unprocessable_entity
+      render :new
     end
   end
-
+  
   def destroy
+    @bookmark = Bookmark.find(params[:id])
     @bookmark.destroy
-    redirect_to list_path(@bookmark.list), status: :see_other
+    redirect_to list_path(@bookmark.list), notice: "Bookmark deleted."
   end
 
   private
 
-  def bookmark_params
-    params.require(:bookmark).permit(:comment, :movie_id)
-  end
-
-  def set_bookmark
-    @bookmark = Bookmark.find(params[:id])
-  end
-
   def set_list
     @list = List.find(params[:list_id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
